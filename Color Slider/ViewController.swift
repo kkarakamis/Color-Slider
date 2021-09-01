bc03a669524da369c61526a694155786e4fd96af
kkarakamis <kutaykarakamis@hotmail.com>
2021-09-01 15:05:33 +0300

    Color Maker according to red green blue sliders

--- Color Slider/ViewController.swift
+++ Color Slider/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorArea: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeAreColor(_ sender: Any) {
+        let redColor : CGFloat = CGFloat((redSlider.value-redSlider.minimumValue) / (redSlider.maximumValue - redSlider.minimumValue))
+        let greenColor : CGFloat = CGFloat((greenSlider.value-greenSlider.minimumValue) / (greenSlider.maximumValue - greenSlider.minimumValue))
+        let blueColor: CGFloat = CGFloat((blueSlider.value-blueSlider.minimumValue) / (blueSlider.maximumValue - blueSlider.minimumValue))
+        
+        
+        colorArea.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    
+    
 }
 
